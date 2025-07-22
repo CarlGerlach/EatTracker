0a0f7bd50ac06b85fd5e9e9638c253c9da6a0323
Carl Erik Gerlach <carlerik.gerlach@schueler.max-eyth-schule.de>
2025-07-22 12:43:33 +0200

    - Created first table - Connection to local mysql server incorrect

---
+++ settings.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+
+
+base = declarative_base() # creates a base class for a database model
+
+
+url = "mysql+pymysql://root:password@localhost:3306/mydb"
+engine = create_engine(url, echo=True)
+session = Session(engine) #connection to database
+
+#Classes
+
+class ingredients(base):
+    __tablename__ = 'ingredients'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    energy = Column(Integer)
+    carbs = Column(Integer)
+    sugars = Column(Integer)
+    protein = Column(Integer)
+
+
+
+
+base.metadata.create_all(engine)
